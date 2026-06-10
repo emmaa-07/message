32251762f48f77d1971ec1e4690bb77cb6110df9
Emnaa <emnadouihech2007@gmail.com>
2026-06-10 19:15:45 +0100

    background updated

--- tiny.js
+++ tiny.js
@@ -1,15 +1,14 @@
 window.onload = function() {
     const btn = document.getElementById('btn');
     const card = document.getElementById('card');
-
-
+    
     btn.addEventListener('click', () => {
+        
         btn.style.display = 'none';
         card.style.display = 'block';
+
         setTimeout(() => {
             card.style.display = 'block';
-            btn.style.display = 'none';
         }, 300);
-    }
-    );
+    });
 }
