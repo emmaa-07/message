07b4b8c609a68756cf53a8f916a7ddaf84f09278
Emnaa <emnadouihech2007@gmail.com>
2026-06-11 16:19:06 +0100

    Card

--- tiny.js
+++ tiny.js
@@ -1,6 +1,35 @@
 window.onload = function() {
     const btn = document.getElementById('btn');
     const card = document.getElementById('card');
+
+
+
+    function createLeaf() {
+        const leaf = document.createElement("div");
+
+        leaf.classList.add("leaf");
+        leaf.style.zIndex = "-"+Math.round(Math.random()*3-1)
+        leaf.style.backgroundImage = "url(Media/petal"+Math.round(Math.random()*6)+".png)";
+
+        leaf.style.left =
+            Math.random() * window.innerWidth + "px";
+
+        leaf.style.fontSize =
+            (15 + Math.random() * 20) + "px";
+
+        leaf.style.animationDuration =
+            (5 + Math.random() * 5) + "s";
+
+        leaf.style.setProperty(
+            "--drift",
+            (Math.random() * 300 - 150) + "px"
+        );
+
+        container.appendChild(leaf);
+
+        setTimeout(() => leaf.remove(), 10000);
+    }
+    setInterval(createLeaf,400)
     
     btn.addEventListener('click', () => {
         
